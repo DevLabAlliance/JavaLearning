78c5868a82afb84bf9609b96ca975d9855dde494
devan <devan@UPLC>
2020-05-03 16:39:10 +0530

    programs

--- src/com/dla/CharactersInAString.java
+++ src/com/dla/CharactersInAString.java
@@ -1,39 +1,27 @@
 package com.dla;
 
-
-
 /**
- * @author  Devlabs Alliance
+ * @author DevLabs Alliance
  *
  */
-public class CharactersinaString {
+//java program to count total no of vowels and consonant in string
 
+public class CharactersInAString {
 
-
-//java program to  count total no of vowels and consonant in string
-
-
-	public static void main(String[] args)
-	{
-		int vcount=0,ccount=0;
-		String str="This is the length of string character";
-		str=str.toLowerCase();
-		for(int i=0;i<str.length();i++)
-		{
-			if(str.charAt(i)=='a'||str.charAt(i)=='e'||
-					str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u')
-			{
+	public static void main(String[] args) {
+		int vcount = 0, ccount = 0;
+		String str = "This is the length of string character";
+		str = str.toLowerCase();
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+					|| str.charAt(i) == 'u') {
 				vcount++;
+			} else if ((str.charAt(i) >= 'a' && str.charAt(i) <= 'z')) {
+				ccount++;
 			}
-					else if ((str.charAt(i)>='a'&&str.charAt(i)<='z'))
-							{
-						ccount++;
-							}
 		}
-		System.out.println("No of vowels "+vcount);
-		System.out.println("No of consonants "+ccount);
-		
+		System.out.println("No of vowels " + vcount);
+		System.out.println("No of consonants " + ccount);
+
 	}
 }
-
-
